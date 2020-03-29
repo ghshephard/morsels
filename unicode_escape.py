3d121342b41b5e1b885b41bcaa4cb107aaf6abca
Gordon Shephard <gordon@shephard.org>
2020-03-29 17:15:43 -0400

    argparse.FileType for the win!

--- unicode_escape.py
+++ unicode_escape.py
@@ -2,31 +2,30 @@ import argparse
 import sys
 
 
+def escape(c, style):
+    if ord(c) <= 127:
+        ch = c
+    elif style == 'html':
+        ch = f'&#x{ord(c):x};'
+    elif ord(c) < 65535:
+        ch = f'\\u{ord(c):04x}'
+    else:
+        ch = f'\\U{ord(c):08x}'
+    return ch
+
+
 def parse_file(fp, style):
-    out = ''
-    for c in fp.read():
-        if ord(c) <= 127:
-            ch = c
-        elif style == 'html':
-            ch = f'&#x{ord(c):x};'
-        elif ord(c) < 65535:              
-            ch = f'\\u{ord(c):04x}'
-        else:
-            ch = f'\\U{ord(c):08x}'
-        out += ch
-    print(out, end="")
+    print(''.join(escape(c, style) for c in fp.read()), end="")
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description='parse utf-8 files')
-    parser.add_argument('filename')
+    parser.add_argument('file', type=argparse.FileType('rt'))
     parser.add_argument('--style', default='default', 
                         choices=['default', 'html'])
     args = parser.parse_args()
-    fn = args.filename
-    if fn == '-':
-        fp = sys.stdin
-    else:  
-        fp = open(fn, 'r', encoding='utf-8')
+    parse_file(args.file, args.style)
 
-    parse_file(fp, args.style)
+
+if __name__ == "__main__":
+    main()
