cef5fb41771f7d34a06cc6f40d7bf8cac685d85d
Gordon Shephard <gordon@shephard.org>
2020-05-31 00:56:12 -0400

    added len function

--- float_range.py
+++ float_range.py
@@ -1,3 +1,5 @@
+import math
+
 class float_range:
     def __init__(self, start, stop=None, step=None):
         if stop is None:
@@ -11,19 +13,33 @@ class float_range:
         else:
             self.step = step
 
-    def __iter__(self):
-        cur = self.start
-        while not self._done(cur, self.start, self.stop):
-            yield cur
-            cur += self.step
-
-    def _done(self, cur, start, stop):
+    def _done(self, cur, start, stop, step):
         if start < stop and cur < stop:
-            return False
+            if step > 0:
+                return False
+            else:
+                return True
         elif start > stop and cur > stop:
-            return False
+            if self.step < 0:
+                return False
+            else:
+                return True
         else:
             return True
+
+    def __len__(self):
+
+        len = math.floor((self.stop - self.start) / self.step) + 1
+        if len >= 0:
+            return len
+        else:
+            return 0
+
+    def __iter__(self):
+        cur = self.start
+        while not self._done(cur, self.start, self.stop, self.step):
+            yield cur
+            cur += self.step
 
 
 if __name__ == "__main__":
@@ -31,8 +47,12 @@ if __name__ == "__main__":
         (0.5, 2.5, 0.5),
         (3.5, 0, -1),
         (0.0, 3.0, None),
-        (3.0, None, None,)
+        (3.0, None, None),
+        (5, 10, 1.5),
+        (10, 5, 1.5),
+        (10, 5, -1.5),
+        (5, 10, -1.5),
     )
     for start, stop, end in tstcon:
         fr = float_range(start, stop, end)
-        print(list(fr))
+        print( [start,stop,end], len(fr),list(fr))
