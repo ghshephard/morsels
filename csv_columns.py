7f3373d7879a074a1314a7ab7ba63d6bb8fa16e9
Gordon Shephard <gordon@shephard.org>
2020-07-04 16:06:12 -0400

    Add restval
    DictReader has an option to fill out missing lines with
    a custom value - otherwise defaults to None.

--- csv_columns.py
+++ csv_columns.py
@@ -1,17 +1,18 @@
-def csv_columns(f, headers=None):
+def csv_columns(f, headers=None, missing=None):
     import csv
     from collections import defaultdict
     csvdic = defaultdict(list)
-    reader = csv.DictReader(f)
+    reader = csv.DictReader(f, restval=missing)
     if headers is None:
         headers = reader.fieldnames
     else:
         csvdic = {k: [h] for k, h in zip(headers, reader.fieldnames)}
     for row in reader:
+
         for k, v in zip(headers, row.values()):
             csvdic[k].append(v)
     return(csvdic)
 
 
-rep = csv_columns(open('foo.csv'), headers=['HEAD1','HEAD2'])
+rep = csv_columns(open('foo.csv'), headers=['HEAD1','HEAD2'], missing=0)
 print(rep)
