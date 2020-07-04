49c64cf46b9c83b942f85dd969d5e0c22cf1db5c
Gordon Shephard <gordon@shephard.org>
2020-07-04 16:28:15 -0400

    Use the fieldnames= option
    with DictReader, if you specify fieldnames=, you the reader
    will read the first row as a normal data field.

--- csv_columns.py
+++ csv_columns.py
@@ -2,14 +2,9 @@ def csv_columns(f, headers=None, missing=None):
     import csv
     from collections import defaultdict
     csvdic = defaultdict(list)
-    reader = csv.DictReader(f, restval=missing)
-    if headers is None:
-        headers = reader.fieldnames
-    else:
-        csvdic = {k: [h] for k, h in zip(headers, reader.fieldnames)}
+    reader = csv.DictReader(f, restval=missing, fieldnames=headers)
     for row in reader:
-
-        for k, v in zip(headers, row.values()):
+        for k, v in row.items():
             csvdic[k].append(v)
     return(csvdic)
 
