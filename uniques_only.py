4bcd93893424d5d9ffa1918c1f3830a06b9d7693
Gordon Shephard <gordon@shephard.org>
2020-07-14 09:30:15 -0400

    Added ability to support hashability
    Uses sets and lists to provide high-performance methods
    for testing presence of items that are hashable, but still
    support non-hashable items with lists.

--- uniques_only.py
+++ uniques_only.py
@@ -1,17 +1,35 @@
+'''provides functions to list unique items'''
+from collections.abc import Hashable
+
+
 def uniques_only(incoming):
+    '''Return list of Unique Items'''
     chkset = set()
+    chklst = []
     for i in incoming:
-        if i not in chkset:
+        found = False
+        if isinstance(i, Hashable):
+            if i in chkset:
+                found = True
+            else:
+                chkset.add(i)
+        else:
+            if i in chklst:
+                found = True
+            else:
+                chklst.append(i)
+        if not found:
             yield i
-        chkset.add(i)
 
 
 if __name__ == "__main__":
     nums = [1, -3, 2, 3, -1]
     squares = (n**2 for n in nums)
-    chklst = [
+    trylst = [
+        [['a', 'b'], ['a', 'c'], ['a', 'b']],
         [1, 2, 2, 1, 1, 3, 2, 1],
-        squares
+        squares,
+        [['a', 'b'], ['a', 'c'], ['a', 'b']]
         ]
-    for chk in chklst:
+    for chk in trylst:
         print(list(uniques_only(chk)))
