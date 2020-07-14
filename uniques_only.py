28c5120959af67968524461b96e51eb150687b85
Gordon Shephard <gordon@shephard.org>
2020-07-14 08:26:13 -0400

    Doh - chkset never bult.

--- uniques_only.py
+++ uniques_only.py
@@ -3,8 +3,7 @@ def uniques_only(incoming):
     for i in incoming:
         if i not in chkset:
             yield i
-        else:
-            chkset.add(i)
+    chkset.add(i)
 
 
 if __name__ == "__main__":
