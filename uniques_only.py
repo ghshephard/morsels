9e87093c1432d9590eb6fa39f49022fb263732fe
Gordon Shephard <gordon@shephard.org>
2020-07-14 08:19:05 -0400

    First Draft.  Simple case.

---
+++ uniques_only.py
@@ -0,0 +1,18 @@
+def uniques_only(incoming):
+    chkset = set()
+    for i in incoming:
+        if i not in chkset:
+            yield i
+        else:
+            chkset.add(i)
+
+
+if __name__ == "__main__":
+    nums = [1, -3, 2, 3, -1]
+    squares = (n**2 for n in nums)
+    chklst = [
+        [1, 2, 2, 1, 1, 3, 2, 1],
+        [squares]
+        ]
+    for chk in chklst:
+        print(list(uniques_only(chk)))
