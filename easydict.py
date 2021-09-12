44d995b26717c81aacf435020de3d31fc2ce44ec
Gordon Shephard <gordon@shephard.org>
2021-09-11 23:30:29 -0400

    Normalization of keys
    Having access to the __getitem__ / __setitem__ is like a super power.
    You can modify the key name before storing it in the __dict__
    which automatically grants it an existance as an attribute on the
    class.   Need to keep in mind the difference between hasattr and
    item.

--- easydict.py
+++ easydict.py
@@ -1,17 +1,42 @@
+
 class EasyDict(dict):
-    def __init__(self, *args, **kwargs):
+    def _norm(self, k):
+        if self.normalize:
+            return k.replace(" ","_")
+        else:
+            return k
+
+    def _normdic(self, dic):
+        return {self._norm(k):v for k,v in dic.items()}
+
+    def __init__(self, *args, normalize = False, **kwargs):
+        self.normalize=normalize
+        args = ( self._normdic(item ) if isinstance(item, dict) else item for item in args)
+        kwargs = self._normdic(kwargs)
         self.__dict__.update(*args, **kwargs)
+
     def __getitem__(self, key):
-        return self.__dict__[key]
+        return self.__dict__[self._norm(key)]
+
     def __setitem__(self, key, value):
-        self.__dict__[key] = value
+        self.__dict__[self._norm(key)] = value
+
     def __repr__(self):
         return str(self.__dict__)
+
     def __eq__(self, other):
         if self.__dict__ == other.__dict__:
             return True
         else:
             return False
+
+    def __ne__(self, other):
+        if self.__dict__ != other.__dict__:
+            return True
+        else:
+            return False
+    def get(self, key, default = None):
+        return self.__dict__.get(self._norm(key),default)
         
 
 if __name__ == "__main__":
@@ -24,4 +49,14 @@ if __name__ == "__main__":
     person = EasyDict(name="Trey Hunner", location="San Diego")
     assert person.location == 'San Diego'
     assert (person == EasyDict(name="Trey", location="San Diego")) == False
+    assert (person != EasyDict(name="Gordon")) == True
     assert (person == EasyDict(name="Trey Hunner", location="San Diego")) == True
+    assert person.get('profession') == None
+    assert person.get('profession', 'unknown') == "unknown"
+    assert person.get('name', 'unknown') == 'Trey Hunner'
+    person = EasyDict(name="Trey Hunner", location="San Diego", normalize=True)
+    person['company name'] = "Truthful Technology LLC"
+    assert person.company_name == "Truthful Technology LLC"
+    assert person['company name'] == "Truthful Technology LLC"
+    person = EasyDict({'name':'Gordon Shephard', 'first job':'Mcdonalds'}, normalize=True)
+    assert person.first_job == 'Mcdonalds'
