f7db4adc3abdd87bc9647f6d3ddb454c3574c487
Gordon Shephard <gordon@shephard.org>
2021-09-14 01:00:39 -0400

    format_ranges - sorted dupes minmal length I genuinely feel like I went off the reservation on this one, I managed to solve it - but it was through a combination of index hacking, poping out array elements, and generally doing all the things in python tht I am not supposed to do.  About the only thing I was happy about was the sorted by first character and then range - nailed the lambda without even really thinking about it. One interesting lesson that I (should not have) learned from this exercise is the concept of breaking down a problem to its simpler components before finally solving the hard ones.  The mental burden of trying to pretty print ranges had already been completed earlier on, so I was left free to focus on the complex parts of running down dupes. I do wish I knew what difficulty ranking this bonus question was - will be very interested to see how Tres solved it.

--- format_ranges.py
+++ format_ranges.py
@@ -1,16 +1,34 @@
-def format_ranges(numbers):
-    lst=[]
-    iterator = iter(numbers)
-    first = current = next(iterator)
-    for i in iterator:
-        if i > current + 1:
-            lst.append( (first,current))
-            first = i
-        current = i
-    lst.append( ( first, current ))
-    ret = ",".join( f'{a}-{b}' if b>a else f'{a}' for (a,b) in lst )    
-    return ret
+from collections import Counter
 
+def format_ranges(numbers):
+    def getnum(lst, index):
+        num,c = lst[index]
+        if c > 1:
+            lst[index]=(num,c-1)
+            index += 1
+        else:
+            lst.pop(index)
+        if index >= len(lst):
+            index = 0 
+        return index, num
+
+    lst = sorted(Counter(numbers).items())
+    index = 0
+
+    index, num = getnum(lst, index)
+    first = current = num
+    nums = []
+    while lst:
+        index, num  = getnum(lst, index)
+        if num != current + 1:
+            nums.append((first, current))
+            first = num 
+        current = num
+
+    nums.append((first, current))
+    srtnums = sorted(nums, key = lambda x:(x[0],x[1]-x[0]))
+    ret = ",".join( f'{a}-{b}' if b>a else f'{a}' for (a,b) in srtnums )    
+    return ret
 
 if __name__ == "__main__":
     assert (format_ranges([1, 2, 3, 4, 5, 6, 7, 8])) == '1-8'
@@ -18,5 +36,8 @@ if __name__ == "__main__":
     numbers = [3, 4, 15, 16, 17, 19, 20]
     assert (format_ranges(n+1 for n in numbers)) == '4-5,16-18,20-21'
     assert(format_ranges([4] )) == '4'
+    assert(format_ranges([1, 3, 5, 6, 8])) == '1,3,5-6,8'
+    assert(format_ranges([9, 1, 7, 3, 2, 6, 8]) == "1-3,6-9")
+    assert(format_ranges([1, 9, 1, 7, 3, 8, 2, 4, 2, 4, 7])) == '1-2,1-4,4,7,7-9'
     assert(format_ranges([1, 3, 5, 6, 8])) == '1,3,5-6,8'
 
