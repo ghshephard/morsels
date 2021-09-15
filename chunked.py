53837e4b42e98ee2e7b52030b3d73be4c45fcfa5
Gordon Shephard <gordon@shephard.org>
2021-09-15 01:29:59 -0400

    chunked - with support for fill and any iterable. I'm guessing there is a more clever way of doing this. Things I like:   o use of yield across the board.   o use of iterator / StopIteration   o use of the while iterator to wait for the iterator to be emptied.
    Things I should have done:
      o Should always create a NOTHING = object() to avoid
         the screwing up on None as a valid arg trap.
    
    Things I don't like:
       o Building the ret by appending values from the itertor.
          It really feels like I should be able to use a list comprehension
          here - but when I tried it - didn't work.
    
    Things I'm not sure of:
      o was there a cleaner way of exiting than to check to see if
         ret was None?

--- chunked.py
+++ chunked.py
@@ -1,12 +1,28 @@
-def chunked(sequence, n):
-     for i in range(0,len(sequence),n):
-         yield list(sequence[i:i+n])
-
+NOTHING = object()
+def chunked(sequence, n, *, fill=NOTHING):
+    iterator=iter(sequence)
+    
+    try:
+        while iterator:
+            ret = []
+            for i in range(n):
+                ret.append(next(iterator))
+            yield ret
+    except StopIteration:
+        if ret:
+            yield ret+[fill]*(n-len(ret)) if fill is not NOTHING else ret 
+ 
+    
 if __name__ == "__main__":
     for chunk in chunked([1,2,3,4,5],n=2):
         print(*chunk)
     for chunk in chunked(range(10), 4):
         print(tuple(chunk))
-    # squares = (n**2 for n in range(6))
-    # for chunk in chunked(squares, 3):
-    #     print(*chunk)
+    squares = (n**2 for n in range(6))
+    for chunk in chunked(squares, 3):
+        print(*chunk)
+    for chunk in chunked(range(10), 4, fill=0):
+        print(*chunk)
+
+    for chunk in chunked(range(4), 2, fill=0):
+        print(*chunk)
