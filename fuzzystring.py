db0d13d44ec66c684c53aa3783258087975d1a5b
Gordon Shephard <gordon@shephard.org>
2021-09-26 23:47:21 -0400

    First bonus straight forward  Always remember - it's le not lte

--- fuzzystring.py
+++ fuzzystring.py
@@ -1,26 +1,34 @@
-from functools import total_ordering
-
-@total_ordering
 class FuzzyString(str):
     def __init__(self, intext):
         self.intext = intext
-
     def __eq__(self, other):
         return self.intext.lower() == other.lower()
-
+    def __ne__(self,other):
+        return self.intext.lower() != other.lower()
     def __lt__(self, other):
         return self.intext.lower() < other.lower()
-
-    # def __ne__(self, other):
-    #     return not self.intext == other
+    def __gt__(self,other):
+        return self.intext.lower() > other.lower()
+    def __ge__(self,other):
+        return self.intext.lower() >= other.lower()
+    def __le__(self,other):
+        return self.intext.lower() <= other.lower()
         
 
+
 a=FuzzyString('abc')
-b=FuzzyString('bcd')
+b=FuzzyString('BCD')
 
-print(a > b)
-# greeting = FuzzyString('Hey TREY!')
-# assert (greeting == 'hey Trey!') == True
-# assert ( greeting == 'heyTrey') == False
-# assert ( greeting != 'heyTrey') == True
-
+greeting = FuzzyString('Hey TREY!')
+assert (greeting == 'hey Trey!') == True
+assert ( greeting == 'heyTrey') == False
+assert ( greeting != 'heyTrey') == True
+o_word = FuzzyString('Octothorpe')
+assert ('hashtag' < o_word) == True
+assert ('hashtag' > o_word) == False
+tokyo = FuzzyString("tokyo")
+toronto = FuzzyString("TORONTO")
+assert (tokyo < toronto) == True
+assert (toronto < tokyo) == False
+apple = FuzzyString('Apple')
+assert  (apple >= "animal")
