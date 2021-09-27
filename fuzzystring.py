b2bb7990225b66deeb9728979344b3f7a6152aa0
Gordon Shephard <gordon@shephard.org>
2021-09-27 00:12:53 -0400

    unicode normalization This will be a handy thing to keep in my back pocket - Normalizing unicode so that it can be properly compared.

--- fuzzystring.py
+++ fuzzystring.py
@@ -1,8 +1,14 @@
+import unicodedata
+
 class FuzzyString(str):
+
+    def _nc(self, text):
+        return unicodedata.normalize("NFKD", text.casefold())
+
     def __init__(self, intext):
         self.intext = intext
     def __eq__(self, other):
-        return self.intext.lower() == other.lower()
+        return self._nc(self.intext.lower()) == self._nc(other.lower())
     def __ne__(self,other):
         return self.intext.lower() != other.lower()
     def __lt__(self, other):
@@ -10,9 +16,15 @@ class FuzzyString(str):
     def __gt__(self,other):
         return self.intext.lower() > other.lower()
     def __ge__(self,other):
+        
         return self.intext.lower() >= other.lower()
     def __le__(self,other):
         return self.intext.lower() <= other.lower()
+    def __contains__(self, other):
+        return self._nc(other.lower()) in self._nc(self.intext.lower())
+    def __add__(self, other):
+        return FuzzyString("".join([self.lower(),other]))
+        
         
 
 
@@ -32,3 +44,8 @@ assert (tokyo < toronto) == True
 assert (toronto < tokyo) == False
 apple = FuzzyString('Apple')
 assert  (apple >= "animal")
+ss = FuzzyString('ss')
+assert('\u00df' == ss)
+e = FuzzyString('\u00e9')
+assert('\u0065\u0301' == e)
+assert('\u0301' in e)
