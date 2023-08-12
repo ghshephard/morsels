5c9fdf6700d772e2f4baedea92207ed4a43db24b
Gordon Shephard <gshephard@roblox.com>
2023-08-12 14:13:38 -0700

    Multiple Keys - keyword only parser
    Python has this weird *search keyword only parser feature that landed
    back in the early days of python 3.0.   It's a way to have a single
    parameter that can have mutiple arguments - landing in *search
    in this case.   I kludge a solution here by checking to see if
    it has just one key - in which case I convert from a tuple ->
    string, and then continue on the normal path *else*
    I return a TUPLE of a GENERATIOR function which
    combines all of the elements of the tuple multi
    argument parameter.  Horrible.

--- second_round/dicttools.py
+++ second_round/dicttools.py
@@ -1,16 +1,20 @@
 """Dictools for pymorsels"""
 SENTINEL=object()
 
-def pluck(indic, search, sep=".", default=SENTINEL):
+def pluck(indic, *search, sep=".", default=SENTINEL):
     """Search a dictionary on search and return value"""
-    keys = search.split(sep)
-    if default==SENTINEL:
-        return indic[keys[0]] if len(keys)==1 else pluck(indic[keys[0]], ".".join(keys[1:]))
-    else:
-        try:
+    if len(search) == 1:
+        search=search[0]
+        keys=search.split(sep)
+        if default==SENTINEL:
             return indic[keys[0]] if len(keys)==1 else pluck(indic[keys[0]], ".".join(keys[1:]))
-        except KeyError:
-            return default
+        else:
+            try:
+                return indic[keys[0]] if len(keys)==1 else pluck(indic[keys[0]], ".".join(keys[1:]))
+            except KeyError:
+                return default
+    else:
+        return tuple((pluck(indic,skey, sep=sep, default=default) for skey in search))
 
 
 def main():
@@ -22,6 +26,9 @@ def main():
     assert(pluck(data, 'category/name', sep='/')) == "Music"
     data = {'amount': 10.64, 'category': {'name': 'Music', 'group': 'Fun'}}
     assert(pluck(data, 'category.created', default='N/A')) == "N/A"
+    data = {'amount': 10.64, 'category': {'name': 'Music', 'group': 'Fun'}}
+    print(pluck(data, 'category.name', 'amount'))
+    assert(pluck(data, 'category.name', 'amount')) == ("Music",10.64)
     print("All Tests Passed!")
 
 if __name__ == "__main__":
