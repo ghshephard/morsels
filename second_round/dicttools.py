acb3a47a4cc8aca1c3f55e0b2caf0b9ceefb3cc3
Gordon Shephard <gshephard@roblox.com>
2023-08-12 13:52:58 -0700

    Adding a default
    Kind of kludgey - check to see if one is set with Object() Sentinel hack.
    Also - two returns - ugly - want to refactor it out cleanly.

--- second_round/dicttools.py
+++ second_round/dicttools.py
@@ -1,8 +1,16 @@
 """Dictools for pymorsels"""
-def pluck(indic, search, sep="."):
+SENTINEL=object()
+
+def pluck(indic, search, sep=".", default=SENTINEL):
     """Search a dictionary on search and return value"""
     keys = search.split(sep)
-    return indic[keys[0]] if len(keys)==1 else pluck(indic[keys[0]], ".".join(keys[1:]))
+    if default==SENTINEL:
+        return indic[keys[0]] if len(keys)==1 else pluck(indic[keys[0]], ".".join(keys[1:]))
+    else:
+        try:
+            return indic[keys[0]] if len(keys)==1 else pluck(indic[keys[0]], ".".join(keys[1:]))
+        except KeyError:
+            return default
 
 
 def main():
@@ -11,7 +19,9 @@ def main():
     assert(pluck(data, 'amount')) == 10.64
     assert(pluck(data, 'category.group')) == "Fun"
     data = {'amount': 10.64, 'category': {'name': 'Music', 'group': 'Fun'}}
-    assert(pluck(data, 'category/name', sep='/') == "Music")
+    assert(pluck(data, 'category/name', sep='/')) == "Music"
+    data = {'amount': 10.64, 'category': {'name': 'Music', 'group': 'Fun'}}
+    assert(pluck(data, 'category.created', default='N/A')) == "N/A"
     print("All Tests Passed!")
 
 if __name__ == "__main__":
