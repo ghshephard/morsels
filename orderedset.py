ccbf5ad2f0c4dbcca7ab7e7a6f7c78b4501e6438
Gordon Shephard <gshephard@roblox.com>
2023-08-14 22:36:22 -0700

    Added the repr, contains, quick smoke test.

--- orderedset.py
+++ orderedset.py
@@ -1,5 +1,5 @@
 class OrderedSet:
-    def __init__(self, items):
+    def __init__(self, items=[]):
         self._myset={}
         for i in items:
             if not(i in self._myset):
@@ -7,7 +7,23 @@ class OrderedSet:
 
     def __iter__(self):
         return iter(self._myset.keys())
+
+    def __len__(self):
+        return len(self._myset.keys())
+
+    def __repr__(self):
+        return(str(self.__class__.__name__))+"("+str(list(self))+")"
     
+    def __contains__(self,item):
+        return item in self._myset.keys()
     
 
-    
+def main():
+    ordered_words = ['these', 'are', 'words', 'in', 'an', 'order']
+    assert(list(OrderedSet(ordered_words))) == ['these', 'are', 'words', 'in', 'an', 'order']
+    print("All Tests Passed")
+
+
+
+if __name__=="__main__":
+    main()
