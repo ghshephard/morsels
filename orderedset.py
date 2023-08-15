60a31ad4a261e036218fdebad88471629ba4bd93
Gordon Shephard <gshephard@roblox.com>
2023-08-14 22:58:26 -0700

    Added Equality, Add, DIscard

--- orderedset.py
+++ orderedset.py
@@ -2,8 +2,7 @@ class OrderedSet:
     def __init__(self, items=[]):
         self._myset={}
         for i in items:
-            if not(i in self._myset):
-                self._myset[i]=True
+            self.add(i)
 
     def __iter__(self):
         return iter(self._myset.keys())
@@ -17,10 +16,32 @@ class OrderedSet:
     def __contains__(self,item):
         return item in self._myset.keys()
     
+    def __eq__(self, other):
+        if isinstance(other,OrderedSet) and list(self._myset.keys()) == list(other):
+            return True
+        elif isinstance(other,set) and set(self._myset.keys()) == set(other):
+            return True
+        else:
+            return False
+    def add(self,item):
+        if not(item in self._myset):
+            self._myset[item]=True
+
+    def discard(self, item):
+        try:
+            self._myset.pop(item)
+        except KeyError:
+            pass
+
 
 def main():
     ordered_words = ['these', 'are', 'words', 'in', 'an', 'order']
     assert(list(OrderedSet(ordered_words))) == ['these', 'are', 'words', 'in', 'an', 'order']
+    s2=OrderedSet([5,2,1,4,3])
+    s2.add(7)
+    assert(list(s2))==[5,2,1,4,3,7]
+    s2.discard(5)
+    assert(list(s2)==[2,1,4,3,7])
     print("All Tests Passed")
 
 
